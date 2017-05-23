29f6a2b001fcbc32878f2d43af936829e11b0685
Bien Pham <pham_bv@hotmail.com>
2017-05-23 13:57:46 -0400

    Mark selected note in list

--- imports/ui/NoteList.js
+++ imports/ui/NoteList.js
@@ -4,15 +4,21 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
+import { Session } from 'meteor/session';
 
 import { Notes } from '../api/notes';
 import NoteListHeader from './NoteListHeader';
 import NoteListItem from './NoteListItem';
+import NoteListEmptyItem from './NoteListEmptyItem';
+
+
+
 
 export const NoteList = (props) => {
     return (
         <div>
             <NoteListHeader />
+            { props.notes.length === 0 ? <NoteListEmptyItem/> : undefined }
             { props.notes.map((note) => {
                 return <NoteListItem key={note._id} note={note}/>
             })}
@@ -25,10 +31,16 @@ NoteList.propTypes = {
   notes: React.PropTypes.array.isRequired
 };
 export default createContainer(() => {
+    const selectedNoteId = Session.get('selectedNoteId');
     Meteor.subscribe('notes');
 //keys in here are props in the component
     return {
-        notes: Notes.find().fetch()
+        notes: Notes.find().fetch().map((note) => {
+            return {
+                ...note,
+                selected: note._id === selectedNoteId
+            };
+        })
     };
 
 }, NoteList);
---
+++ imports/ui/NoteListEmptyItem.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Bien on 2017-05-23.
+ */
+import React from 'react';
+
+
+export default () => {
+    return (
+        <div>
+            <h5>You have no notes</h5>
+            <p>Create a note to get started!</p>
+        </div>
+    );
+};
